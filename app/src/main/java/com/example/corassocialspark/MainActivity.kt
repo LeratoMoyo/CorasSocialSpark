1bfaba33687ca22eb815458f36964aa82f3d937d
Lerato Moyo <liramoyo46@gmail.com>
2026-03-29 23:29:57 +0200

    code for main activity.

--- app/src/main/java/com/example/corassocialspark/MainActivity.kt
+++ app/src/main/java/com/example/corassocialspark/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.example.corassocialspark
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_welcome)
+
+        val startButton = findViewById<Button>(R.id.startButton)
+
+        startButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
         }
     }
-}
