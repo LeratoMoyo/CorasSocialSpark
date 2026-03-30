d6a835296c92294bcfdde72a7138ebd2a6be1a58
Lerato Moyo <liramoyo46@gmail.com>
2026-03-30 23:16:01 +0200

    completed mainactivity page

--- app/src/main/java/com/example/corassocialspark/MainActivity.kt
+++ app/src/main/java/com/example/corassocialspark/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.corassocialspark
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val input = findViewById<EditText>(R.id.timeInput)
+        val button = findViewById<Button>(R.id.btnSuggest)
+        val result = findViewById<TextView>(R.id.resultText)
+
+        button.setOnClickListener {
+
+            val message = when (userInput) {
+                "morning" -> "🌞Send a good morning message to a family member"
+                "mid-morning" -> "🙏Thank a colleague with a quick message"
+                "afternoon" -> "\uD83E\uDD2A Share a funny meme with a friend"
+                "afternoon snack time" -> "\uD83D\uDE18 Send a thinking of you message"
+                "dinner" -> "☎\uFE0F Call a friend for a 5-minute catch-up"
+                "night, after dinner" -> "\uD83C\uDF1D Leave a thoughtful comment on a post"
+
+                "" -> "Please enter a time of day😏"
+
+                else -> "Try: morning, mid-morning, afternoon, dinner, or night"
+            }
+            result.text = message
+            }
         }
-    }
-}
+
+        }
